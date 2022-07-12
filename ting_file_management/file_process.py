4d035f991483556e2a3911a49b27c4ae3526085f
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-12 19:21:51 -0300

    feat: retorna dados no stdout

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    name = path_file
+    file_content = []
+    with open(path_file, mode="r") as file:
+        for row in file:
+            instance.enqueue(row)
+    for index in range(len(instance)):
+        file_content.append(instance.search(index).strip())
+    output = {
+        "nome_do_arquivo": name,
+        "qtd_linhas": len(file_content),
+        "linhas_do_arquivo": file_content
+    }
+    print(output)
 
 
 def remove(instance):
