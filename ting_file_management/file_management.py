ccc6dae6281576508631c7ceb0c9c70b0df3b8ff
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-12 17:02:59 -0300

    feat: importa arquivos txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,22 @@
+from sys import stderr
+from subprocess import check_output, CalledProcessError
+
+
+def file_exists(path_file):
+    command = f"ls {path_file} &> /dev/null"
+    try:
+        check_output(command, shell=True)
+        return True
+    except CalledProcessError:
+        return False
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not file_exists(path_file):
+        stderr.write(f"Arquivo {path_file} não encontrado")
+        return None
+    file_content = []
+    with open(path_file, mode="r") as file:
+        for row in file:
+            file_content.append(row.strip())
+    return file_content
