dd29e263cf5b01ee8acef199072039641ba42078
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-12 18:15:56 -0300

    feat: checa extensão do arquivo

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -2,9 +2,20 @@ import sys
 from os.path import exists as file_exists
 
 
+def is_txt(path_file):
+    try:
+        file_extension = path_file.split(".")[1]
+    except IndexError:
+        file_extension = ""
+    return file_extension == "txt"
+
+
 def txt_importer(path_file):
     if not file_exists(path_file):
         print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return None
+    if not is_txt(path_file):
+        print("Formato inválido", file=sys.stderr)
         return None
     file_content = []
     with open(path_file, mode="r") as file:
