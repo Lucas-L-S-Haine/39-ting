ce61a2fc9970daa09807984c296211bfb7c9509e
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-12 21:03:01 -0300

    refactor: deixa de usar a fila para o output

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,14 +1,14 @@
 def process(path_file, instance):
     name = path_file
     file_content = []
+    lines = 0
     with open(path_file, mode="r") as file:
         for row in file:
-            instance.enqueue(row)
-    for index in range(len(instance)):
-        file_content.append(instance.search(index).strip())
+            file_content.append(row.strip())
+            lines += 1
     output = {
         "nome_do_arquivo": name,
-        "qtd_linhas": len(file_content),
+        "qtd_linhas": lines,
         "linhas_do_arquivo": file_content
     }
     print(output)
