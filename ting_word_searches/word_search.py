58adb9143cc8485d59f042d44d14e0862c603c8e
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-13 01:17:30 -0300

    feat: implementa função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    files = []
+    for index in range(len(instance)):
+        file_name = instance.search(index)
+        ocurrences = []
+        line_number = 0
+        with open(file_name, mode="r") as file:
+            for row in file:
+                line_number += 1
+                if word in row.lower():
+                    ocurrences.append({"linha": line_number})
+        if len(ocurrences) > 0:
+            files.append({
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": ocurrences
+            })
+    return files
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    pass
